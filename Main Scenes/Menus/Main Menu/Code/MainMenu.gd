b0af717f3f7ba8088fde3eef7051331c36b950d8
Firezaga <106500958+Firezaga@users.noreply.github.com>
2024-03-31 09:59:34 +1000

    Global and Menus
    Simple singleton for storing data throughout scenes. Implemented main menu and settings, with a quit button

---
+++ Main Scenes/Menus/Main Menu/Code/MainMenu.gd
@@ -0,0 +1,32 @@
+extends Node2D
+
+
+# Called when the node enters the scene tree for the first time.
+func _ready():
+	pass # Replace with function body.
+
+
+# Called every frame. 'delta' is the elapsed time since the previous frame.
+func _process(delta):
+	
+	pass
+
+
+func _on_start_game_button_pressed():
+	pass # Replace with function body.
+
+
+func _on_continue_game_button_pressed():
+	pass # Replace with function body.
+
+
+func _on_load_game_button_pressed():
+	pass # Replace with function body.
+
+
+func _on_settings_button_pressed():
+	Global.settings()
+
+
+func _on_quit_button_pressed():
+	Global.quit_game()
