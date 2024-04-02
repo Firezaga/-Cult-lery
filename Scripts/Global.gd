8d86e1731e0b8a953c81233e07eff2a52ddb2c96
Firezaga <106500958+Firezaga@users.noreply.github.com>
2024-04-02 22:32:46 +1000

    Folder Restructure

---
+++ Scripts/Global.gd
@@ -0,0 +1,60 @@
+extends Node
+
+var SettingsMenu = "res://Scenes/Menus/SettingsMenu.tscn"
+var MainMenu = "res://Scenes/Menus/MainMenu.tscn"
+var current_scene = null
+var image = null
+
+func _ready():
+	var root = get_tree().get_root()
+	current_scene = root.get_child(root.get_child_count() - 1)
+
+func goto_scene(path):
+	# This function will usually be called from a signal callback,
+	# or some other function in the current scene.
+	# Deleting the current scene at this point is
+	# a bad idea, because it may still be executing code.
+	# This will result in a crash or unexpected behavior.
+
+	# The solution is to defer the load to a later time, when
+	# we can be sure that no code from the current scene is running:
+
+	call_deferred("_deferred_goto_scene", path)
+
+
+func _deferred_goto_scene(path):
+	# It is now safe to remove the current scene
+	current_scene.free()
+	
+	# Load the new scene.
+	var s = ResourceLoader.load(path)
+	
+	# Instance the new scene.
+	current_scene = s.instantiate()
+	
+	# Add it to the active scene, as child of root.
+	get_tree().get_root().add_child(current_scene)
+	
+	# Optionally, to make it compatible with the SceneTree.change_scene() API.
+	get_tree().set_current_scene(current_scene)
+
+
+func save():
+	pass #todo
+
+
+func load():
+	pass #todo
+
+
+func settings():
+	goto_scene(SettingsMenu)
+
+
+func main_menu():
+	goto_scene(MainMenu)
+
+
+# Used to quit game
+func quit_game():
+	get_tree().quit()
